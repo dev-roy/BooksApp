f244adf8699e048f74e586610d9db55aed3b4094
Rodrigo <rbuendiaramos4100@mtmercy.edu>
2020-04-16 22:58:42 -0400

    Add unit test for filter method

--- BooksAppTests/BooksAppTests.swift
+++ BooksAppTests/BooksAppTests.swift
@@ -19,10 +19,36 @@ class BooksAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testCreateNewBook() {
+    func testFilterFavorites() {
         let book = Book()
+        book.title = "A Song of Fire and Ice"
         book.author = "George R. Martin"
+        book.thumbnailURL = "www.google.com"
+        book.description = "Very good book"
+        book.publisher = "Publishing House"
+        book.isFavorite = false
         
+        let favoriteBook = Book()
+        favoriteBook.title = "A Song of Fire and Ice"
+        favoriteBook.author = "George R. Martin"
+        favoriteBook.thumbnailURL = "www.google.com"
+        favoriteBook.description = "Very good book"
+        favoriteBook.publisher = "Publishing House"
+        favoriteBook.isFavorite = false
+        
+        var booksArray = [book]
+        let bookModelArray = [favoriteBook]
+        
+        for book in booksArray {
+            for bookModel in bookModelArray {
+                if book.title == bookModel.title && book.author == bookModel.author {
+                    book.isFavorite = true
+                }
+            }
+            booksArray.append(book)
+        }
+        
+        XCTAssertTrue(booksArray[1].isFavorite)
     }
 
 }
